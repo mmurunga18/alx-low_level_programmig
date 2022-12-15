a4e9114c35aa6c91239a85a567584de18589d88e
Mercymurunga <mmmemush8@gmail.com>
2022-12-15 00:30:16 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print _alphabet -function to print alpha lower case
+ *
+ * Return; Always 0
+ */
+void print _alphabet(void)
+}
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n');
+}
+
