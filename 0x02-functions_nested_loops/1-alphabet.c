30ef0f5afb9f86d85e872ed4571ab089cb1ad21d
Mercymurunga <mmmemush8@gmail.com>
2023-01-03 04:21:54 -0800

    strncpy

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
+
 /*
-*print_alphabet - Check description
-*Description: It prints the alphabet in lowercase followed by a new line
-*Return: Nothing.
+ *print_alphabet - Check description
+ *Description;It prints the alphabet in lowercase followed by a new line
+ *Return: Always 0
 */
 void print_alphabet(void)
 {
 	char i;
-
 	for (i = 'a'; i <= 'z'; i++)
-	
-       	{	
-	 _putchar(i);
+   	{
+        	_putchar(i);
         }
-         _putchar('\n');
+    	_putchar('\n');
 }
